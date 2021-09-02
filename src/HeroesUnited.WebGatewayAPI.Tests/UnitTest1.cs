f99f7f3964218922f986488081fc31ee3caa7802
Vivek Bure <vivekbure@gmail.com>
2021-09-02 10:02:26 +0530

    HU-1: Pipeline

---
+++ src/HeroesUnited.WebGatewayAPI.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace HeroesUnited.WebGatewayAPI.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var dummy = true;
+            Assert.True(dummy);
+        }
+    }
+}
